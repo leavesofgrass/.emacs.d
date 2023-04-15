dd78d4b9b3c6c75a2094a81e1b7aa99113ecc6f9
Jon Pielaet <jon@pielaet.net>
2023-04-15 15:11:09 -0700

    company doom-modeline ess evil

---
+++ elpa/company-20230415.100/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20230415.100" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "9d55b02c8c4ca823b31ea5c69efa9f8e5ce92c5b" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/evil-20230415.616/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20230415.616" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "f3f5f65091f1c209f52a3b4f38bdfed2729ee0bb" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
