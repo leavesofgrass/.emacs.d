c1df8031eddf1e229e1ef2975b33848809f748d8
Jon Pielaet <jon@pielaet.net>
2023-04-16 10:54:51 -0700

    ess org-ref helm

---
+++ elpa/helm-20230416.635/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20230416.635" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.0")
+    (popup "0.5.3"))
+  :commit "21267aad3c0c32324f0faea17819b9281bbd4256" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
